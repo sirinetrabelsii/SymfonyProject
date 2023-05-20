69447f5557c931ab673d50a799a33669e6e1ffb3
sirine TRABELSI <trabelsii.sirine@gmail.com>
2023-05-20 20:12:44 +0000

    show produit reviews

--- project/src/Controller/ProduitController.php
+++ project/src/Controller/ProduitController.php
@@ -2,19 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\Produit;
 use App\Repository\ProduitRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProduitController extends AbstractController
 {
     #[Route('/produit', name: 'List_Produit')]
-    public function index( ProduitRepository $prodR)
+    public function index(ProduitRepository $prodR)
     {
         // $test="hello world";
         $ListProd = $prodR->findAll();
         // dd($ListProd);
-        return $this->render('produit/index.html.twig',['listeProduits'=> $ListProd] );
+        return $this->render('produit/index.html.twig', ['listeProduits' => $ListProd]);
+    }
+
+    #[Route('/produit/{id}', name: 'show_produit', methods: ['GET'])]
+    public function showProduit(Produit $produit)
+    {
+        dd($produit);
+        $reviews = $produit->getReviews();
+
+        return $this->render('produit/show.html.twig', [
+            'produit' => $produit,
+            'reviews' => $reviews,
+        ]);
     }
 }

7943505fafa7f077498700af5fb441b4a8b2951e
sirine TRABELSI <trabelsii.sirine@gmail.com>
2023-05-26 21:53:29 +0000

    http request

--- project/src/Controller/ProduitController.php
+++ project/src/Controller/ProduitController.php
@@ -6,17 +6,41 @@ use App\Entity\Produit;
 use App\Repository\ProduitRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\HttpClient\HttpClientInterface as SymfonyHttpClientInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class ProduitController extends AbstractController
 {
-    #[Route('/produit', name: 'List_Produit')]
-    public function index(ProduitRepository $prodR)
+    private $client;
+
+    public function __construct(SymfonyHttpClientInterface $client) // Updated type hint
     {
-        // $test="hello world";
-        $ListProd = $prodR->findAll();
-        // dd($ListProd);
-        return $this->render('produit/index.html.twig', ['listeProduits' => $ListProd]);
+        $this->client = $client;
     }
+    #[Route('/produit', name: 'List_Produit')]
+    // public function index(ProduitRepository $prodR)
+    // {
+    //     // $test="hello world";
+    //     $ListProd = $prodR->findAll();
+    //     // dd($ListProd);
+    //     return $this->render('produit/index.html.twig', ['listeProduits' => $ListProd]);
+    // }
+    public function index(ProduitRepository $produitRepository): Response
+    {
+        $res = $this->client->request(
+            'GET',
+            'https://tech.dev.ats-digital.com/api/products?size=50'
+        );
+
+        $data = json_decode($res->getContent(), true);
+      //   dd($data);
+
+        return $this->render('produit/index.html.twig', [
+            'listeProduits' => $data
+        ]);
+    }
+
 
     #[Route('/produit/{id}', name: 'show_produit', methods: ['GET'])]
     public function showProduit(Produit $produit)
